afaa882e5125b32a181efd78d109d567b39baab8
Piorozla <piorozla@o2.pl>
2018-03-11 20:43:03 +0000

    Added root route

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const port = process.env.PORT || 2000;
 
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+  res.status(200).send('API online');
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
