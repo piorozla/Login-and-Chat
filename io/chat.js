6f469102ce75a96cd053f841c3017fc07371a287
Piorozla <piorozla@o2.pl>
2018-04-07 22:03:40 +0100

    Added chat messages

--- io/chat.js
+++ io/chat.js
@@ -1,3 +1,8 @@
+const log = require('./chat.log.json');
+const moment = require('moment');
+// chatSize: how many recent messages to show
+const CHAT_SIZE = 3;
+
 const chat = socket => {
   console.log('a user connected');
   console.log(socket.handshake.session);
@@ -6,8 +11,26 @@ const chat = socket => {
     console.log('user disconnected');
   });
   socket.on('message', msg => {
-    console.log(msg);
+    const logEntry = {
+      date: moment().format('DD:MM:YYYY'),
+      time: moment().format('HH:mm'),
+      entry: msg,
+    };
+    saveMsgToLog(logEntry, moment().format());
+    const chatContent = getChatContent();
+    socket.emit('chat', chatContent);
   });
+};
+
+const saveMsgToLog = (logEntry, time) => {
+  log[time] = logEntry;
+};
+
+const getChatContent = () => {
+  const entries = Object.values(log);
+  const startAt = entries.length - CHAT_SIZE;
+  const chatContent = entries.slice(startAt);
+  return chatContent;
 };
 
 module.exports = chat;
